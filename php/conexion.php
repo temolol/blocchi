ae56aee27c9e1ba1ee3adc5e04914925831c2dfd
temolol <temolol@outlook.com>
2024-10-09 11:43:08 -0700

    ya funnciona la pagina guardar usuario

--- php/conexion.php
+++ php/conexion.php
@@ -9,3 +9,4 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
+echo "Conexión exitosa a la base de datos '$dbname'";
