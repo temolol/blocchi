c5085e9f08574580014e830acb72647e34947b08
temolol <temolol@outlook.com>
2024-10-09 17:24:38 -0700

    verificacion de correo electronico

--- php/crear_usuario.php
+++ php/crear_usuario.php
@@ -1,21 +1,37 @@
 <?php
 include_once "conexion.php";
-
+?>
+<html>
+    <p></p>
+</html>
+<?php
 $nombre = $_POST['nombre'];
 $correo = $_POST['correo'];
 $contrasena = $_POST['contrasena'];
 $apellido = $_POST['apellido'];
 
-$stmt = $conn->prepare("CALL insertar_usuario(?, ?, ?, ?)");
-
-$stmt->bind_param("ssss", $nombre, $apellido, $email, $contrasena,);
-
-if ($stmt->execute()) {
-    echo "Usuario insertado correctamente.";
+// Consulta SQL
+$sql = "SELECT EXISTS(SELECT 1 FROM usuario WHERE email = '$correo')";
+// Ejecutar la consulta
+$resultado = $conn->query($sql);
+// Obtener el valor del resultado
+$fila = $resultado->fetch_row();
+// Verificar si existe el correo
+if ($fila[0] == 1) {
+    echo "El correo ya existe en la base de datos.";
 } else {
-    echo "Error al insertar usuario: " . $stmt->error;
+    $stmt = $conn->prepare("CALL insertar_usuario(?, ?, ?, ?)");
+
+    $stmt->bind_param("ssss", $nombre, $apellido, $correo, $contrasena,);
+    
+    if ($stmt->execute()) {
+        echo "Usuario guardado correctamente.";
+    } else {
+        echo "Error al insertar usuario: " . $stmt->error;
+    }
+    
+    $stmt->close();
+    
+    $conn->close();;
 }
 
-$stmt->close();
-
-$conn->close();
